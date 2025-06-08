c0922ca24046c47c9cf51d05d758e186bfda3fea
mikelsr <mikelsr@pm.me>
2025-06-09 00:25:26 +0200

    Start bot guest code

---
+++ app/guest/bot.go
@@ -0,0 +1,35 @@
+//go:generate env GOOS=wasip1 GOARCH=wasm go build -o bot.wasm bot.go
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	ww "github.com/wetware/pkg/guest/system"
+
+	"github.com/wetware/fbx-demo/app/cap/tiktok"
+)
+
+func main() {
+	ctx := context.Background()
+
+	caps, releases, err := ww.Bootstrap(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		for _, release := range releases {
+			release()
+		}
+	}()
+
+	if len(caps) == 0 {
+		fmt.Println("No capabilities found in bootstrap")
+		os.Exit(1)
+	}
+
+	_ = tiktok.TikTok(caps[0])
+	// tt := tiktok.TikTok(caps[0])
+	// tt.Mention(ctx, params func(tiktok.TikTok_mention_Params) error)
+}
