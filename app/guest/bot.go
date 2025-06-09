6e044247f3ffc7bee899cef009c8022c7f0cd906
mikelsr <mikelsr@pm.me>
2025-06-09 17:55:15 +0200

    Implement main bot loop and methods

--- app/guest/bot.go
+++ app/guest/bot.go
@@ -13,22 +13,144 @@ type bot struct {
 	tt tiktok.TikTok
 }
 
-	caps, releases, err := ww.Bootstrap(ctx)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		for _, release := range releases {
-			release()
+func (b bot) runLoop(ctx context.Context) error {
+	for {
+		// End loop of the context was canceled
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
 		}
-	}()
 
-	if len(caps) == 0 {
-		fmt.Println("No capabilities found in bootstrap")
-		os.Exit(1)
+		// Get the next mention
+		m, err := b.getMention(ctx)
+		if err != nil {
+			if errors.Is(err, errNoComment) {
+				fmt.Println("No comment found, waiting 5s before retrying")
+				time.Sleep(5 * time.Second)
+				continue
+			} else {
+				return fmt.Errorf("Error getting mention: %w", err)
+			}
+		}
+		mention, err := unmarshalComment(m)
+		if err != nil {
+			return fmt.Errorf("Error unmarshaling comment: %w", err)
+		}
+
+		// Get additional context for the mention.
+		// In this case, the rest of the comments in the post.
+		cs, err := b.getComments(ctx, mention.mediaId)
+		if err != nil {
+			if errors.Is(err, errNoComments) {
+				fmt.Printf("No additional comments found in post %s\n", mention.mediaId)
+				cs = []tiktok.Comment{}
+			} else {
+				return fmt.Errorf("Error getting comments: %w", err)
+			}
+		}
+
+		comments := make([]comment, len(cs))
+		for i, c := range cs {
+			comment, err := unmarshalComment(c)
+			if err != nil {
+				return fmt.Errorf("Error unmarshaling comment: %w", err)
+			}
+			comments[i] = comment
+		}
+
+		// Call the LLM model
+		// TODO mikel: add LLM capability. Modify the text for now.
+		reply := fmt.Sprintf("%s in bed", mention.text)
+
+		// Send the reply to the user
+		err = b.replyToComment(ctx, mention, reply)
+		if err != nil {
+			return fmt.Errorf("Error replying to comment: %w", err)
+		}
+
+		// Sanity sleep, don't burn through our API tokens accidentally...
+		time.Sleep(30 * time.Second)
+	}
+}
+
+func (b bot) getMention(ctx context.Context) (tiktok.Comment, error) {
+	f, release := b.tt.Mention(ctx, nil)
+	defer release()
+	select {
+	case <-ctx.Done():
+	case <-f.Done():
+	}
+	if ctx.Err() != nil {
+		return tiktok.Comment{}, ctx.Err()
 	}
 
-	_ = tiktok.TikTok(caps[0])
-	// tt := tiktok.TikTok(caps[0])
-	// tt.Mention(ctx, params func(tiktok.TikTok_mention_Params) error)
+	res, err := f.Struct()
+	if err != nil {
+		return tiktok.Comment{}, err
+	}
+
+	if !res.HasComment() {
+		return tiktok.Comment{}, errNoComment
+	}
+	return res.Comment()
+}
+
+func (b bot) getComments(ctx context.Context, mediaId string) ([]tiktok.Comment, error) {
+	f, release := b.tt.Comments(ctx, func(tt tiktok.TikTok_comments_Params) error {
+		return tt.SetMediaId(mediaId)
+	})
+	defer release()
+
+	select {
+	case <-ctx.Done():
+	case <-f.Done():
+	}
+
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
+	}
+
+	res, err := f.Struct()
+	if err != nil {
+		return nil, err
+	}
+
+	if !res.HasComments() {
+		return nil, errNoComments
+	}
+
+	cl, err := res.Comments()
+	if err != nil {
+		return nil, err
+	}
+
+	comments := make([]tiktok.Comment, cl.Len())
+	for i := 0; i < cl.Len(); i++ {
+		comments[i] = cl.At(i)
+	}
+
+	return comments, nil
+}
+
+func (b bot) replyToComment(ctx context.Context, source comment, reply string) error {
+	f, release := b.tt.Reply(ctx, func(tt tiktok.TikTok_reply_Params) error {
+		err := tt.SetMediaId(source.mediaId)
+		if err != nil {
+			return err
+		}
+		err = tt.SetCommendId(source.id)
+		if err != nil {
+			return err
+		}
+		return tt.SetResponse(reply)
+	})
+	defer release()
+
+	select {
+	case <-ctx.Done():
+	case <-f.Done():
+	}
+
+	return ctx.Err()
 }
