5f2cb3192a1bd6764385cdbb31baf9b863861a4d
mikelsr <mikelsr@pm.me>
2025-06-09 17:50:22 +0200

    Define main function

---
+++ app/guest/main.go
@@ -0,0 +1,45 @@
+//go:generate env GOOS=wasip1 GOARCH=wasm go build -o bot.wasm main.go bot.go errs.go models.go
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	ww "github.com/wetware/pkg/guest/system"
+
+	"github.com/wetware/fbx-demo/app/cap/tiktok"
+)
+
+func main() {
+	ctx := context.Background()
+
+	caps, releases, err := ww.Bootstrap(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		for _, release := range releases {
+			release()
+		}
+	}()
+
+	if len(caps) == 0 {
+		fmt.Println("No capabilities found in bootstrap")
+		os.Exit(1)
+	}
+
+	tt := tiktok.TikTok(caps[0])
+	if err := tt.Resolve(ctx); err != nil {
+		panic(err)
+	}
+
+	bot := bot{
+		tt: tt,
+	}
+	err = bot.runLoop(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error running bot loop: %s", err)
+		os.Exit(1)
+	}
+}
